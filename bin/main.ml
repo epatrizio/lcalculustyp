0b4274df6899134ca7f8044bb61224b83fddbf68
Eric Patrizio <epatrizio@mpns.fr>
2022-11-24 15:51:39 +0100

    Add status color in output console messages

--- bin/main.ml
+++ bin/main.ml
@@ -14,9 +14,9 @@ let lterms_process (lt : lterm) : unit =
       printf "[Typing result] - %s : %s@." (print_term lt) (print_type res);
       printf "[Eval result] %s@." (print_term (eval lt)) (* eval only if typing ok *)
   with
-  | OperationNotFound err -> eprintf "[Typing ERROR -- Operation not found] %s@." err
-  | UnifyError err -> eprintf "[Typing ERROR -- impossible to unify] %s : %s@." (print_term lt) err
-  | VarNotFound -> eprintf "[Typing ERROR -- Unbound variable] %s@." (print_term lt)
-  | NotImplemented err -> eprintf "[Eval WARNING -- Not implemented] %s@." err
+  | OperationNotFound err -> eprintf "\x1b[31m[Typing ERROR -- Operation not found]\x1b[0m %s@." err
+  | UnifyError err -> eprintf "\x1b[31m[Typing ERROR -- impossible to unify]\x1b[0m %s : %s@." (print_term lt) err
+  | VarNotFound -> eprintf "\x1b[31m[Typing ERROR -- Unbound variable]\x1b[0m %s@." (print_term lt)
+  | NotImplemented err -> eprintf "\x1b[33m[Eval WARNING -- Not implemented]\x1b[0m %s@." err
 
 let _ = List.map lterms_process (List.rev lterms)
